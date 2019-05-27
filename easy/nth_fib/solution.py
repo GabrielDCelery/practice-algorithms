4430176a159f767455268ee796caa8893512e9bd
Gabriel Zeller <gabriel.d.celery@gmail.com>
2019-05-27 11:55:05 +0100

    - Solved Fibonacci

---
+++ easy/nth_fib/solution.py
@@ -0,0 +1,17 @@
+# Time complexity O(n)
+# Space complexity O(1)
+
+def get_nth_fib(nth_fib):
+    target_index = nth_fib - 1
+    fib_slice = [0, 1, 1]
+
+    if target_index < 3:
+        return fib_slice[target_index]
+
+    current_index = 2
+    while current_index < target_index:
+        current_index += 1
+        fib_slice[0] = fib_slice[1]
+        fib_slice[1] = fib_slice[2]
+        fib_slice[2] = fib_slice[0] + fib_slice[1]
+    return fib_slice[2]
